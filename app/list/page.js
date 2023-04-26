4dfdb52eaa59fd18858917a86ac3e1567a1dce05
LeeKyuSung <ks5050577@gmail.com>
2023-04-26 23:02:42 +0900

    좋아요 버튼

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,9 @@
+"use client";
+import { useState } from "react";
+
 export default function List() {
   let 상품 = ["토마토", "면", "코코넛"];
+  let [수량, 수량변경] = useState([0, 0, 0]);
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -10,6 +14,16 @@ export default function List() {
             <h4>
               {i} {상품} $40
             </h4>
+            <span> {수량[i]} </span>
+            <button
+              onClick={() => {
+                let copy = [...수량];
+                copy[i]++;
+                수량변경(copy);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
