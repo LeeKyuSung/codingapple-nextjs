a9488a7f8f1e88a605f37ef11a3f4aaa7ee8a552
LeeKyuSung <ks5050577@gmail.com>
2023-04-27 00:41:47 +0900

    글 작성 기능 추가

---
+++ app/forum/write/page.js
@@ -0,0 +1,12 @@
+export default function Write() {
+  return (
+    <div className="p-20">
+      <h2>글쓰기</h2>
+      <form action="/api/write" method="post">
+        <input type="text" name="title" placeholder="제목" required />
+        <input type="text" name="content" placeholder="내용" required />
+        <button type="submit">글쓰기</button>
+      </form>
+    </div>
+  );
+}
---
+++ pages/api/write.js
@@ -0,0 +1,18 @@
+import { connectDB } from "@/app/util/database";
+
+export default async function handler(요청, 응답) {
+  if (요청.method == "POST") {
+    if (!요청.body.title || !요청.body.content) {
+      return 응답.status(400).json({ message: "제목과 내용을 입력해주세요." });
+    }
+    try {
+      const db = (await connectDB).db("forum");
+      let result = await db.collection("post").insertOne(요청.body);
+      return 응답.status(200).redirect(`/forum/${result.insertedId}`);
+    } catch (e) {
+      return 응답.status(500).json({ message: e.message });
+    }
+  } else {
+    return 응답.status(404);
+  }
+}
