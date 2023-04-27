6b9fb4f3d3779b7e70030557c48d04ccd3faac0e
LeeKyuSung <ks5050577@gmail.com>
2023-04-27 21:31:04 +0900

    url 다시 변경

--- app/forum/write/page.js
+++ app/forum/write/page.js
@@ -2,7 +2,7 @@ export default function Write() {
   return (
     <div className="p-20">
       <h2>글쓰기</h2>
-      <form action="/nextjs/api/write" method="post">
+      <form action="/api/write" method="post">
         <input type="text" name="title" placeholder="제목" required />
         <input type="text" name="content" placeholder="내용" required />
         <button type="submit">글쓰기</button>
