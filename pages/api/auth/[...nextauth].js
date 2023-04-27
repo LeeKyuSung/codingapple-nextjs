b4491f84c2c231c9c166dee97defb53f653cc5a9
LeeKyuSung <ks5050577@gmail.com>
2023-04-27 23:50:11 +0900

    production, dev env 설정

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,13 +5,12 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "ec2f7e66e962ed77ff33",
-      clientSecret: "db9dd04f2a613a44c89265e4252e7a5799f2243b",
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId:
-        "938523790700-hkc03p5of8jmkj7kgrce8qt39lunlg09.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   secret: "2q4p23789ohasfldkjh234lkjhsdf",
