063c512b4549658a64c9c5e3fb36d639098fd123
LeeKyuSung <ks5050577@gmail.com>
2023-04-28 02:59:27 +0900

    댓글 표시

--- app/forum/[id]/Comment.js
+++ app/forum/[id]/Comment.js
@@ -1,16 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Comment(props) {
   const [comment, setComment] = useState("");
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`/api/comment?id=${props._id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, []);
   return (
     <div>
       <div>댓글 목록 보여줄 부분</div>
+      {data.map((v) => {
+        return (
+          <div key={v._id}>
+            <div>{v.content}</div>
+            <div>작성자 : {v.author}</div>
+          </div>
+        );
+      })}
+
       <input
         onChange={(e) => {
           setComment(e.target.value);
         }}
+        value={comment}
       />
       <button
         onClick={() => {
@@ -20,7 +38,16 @@ export default function Comment(props) {
               comment: comment,
               _id: props._id,
             }),
-          });
+          })
+            .then((res) => {
+              if (res.status == 200) {
+                return res.json();
+              }
+            })
+            .then((res) => {
+              setData([...data, res]);
+              setComment("");
+            });
         }}
       >
         댓글 등록
--- pages/api/comment.js
+++ pages/api/comment.js
@@ -4,21 +4,28 @@ import { authOptions } from "./auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!session) {
-    res.status(401).json({ message: "로그인이 필요합니다." });
-    return;
-  }
   if (req.method == "POST") {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      res.status(401).json({ message: "로그인이 필요합니다." });
+      return;
+    }
     req.body = JSON.parse(req.body);
     let 저장할거 = {
       content: req.body.comment,
-      parent: new ObjectId(req._id),
+      parent: new ObjectId(req.body._id),
       author: session.user.email,
     };
 
     let db = (await connectDB).db("forum");
     await db.collection("comment").insertOne(저장할거);
-    res.status(200).json({ message: "댓글이 등록되었습니다." });
+    res.status(200).json(저장할거);
+  } else if (req.method == "GET") {
+    let db = (await connectDB).db("forum");
+    let result = await db
+      .collection("comment")
+      .find({ parent: new ObjectId(req.query.id) })
+      .toArray();
+    res.status(200).json(result);
   }
 }
