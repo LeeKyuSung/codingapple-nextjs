db06667083804f08ad32c031e6c9d0ddf74708d3
LeeKyuSung <ks5050577@gmail.com>
2023-04-27 23:27:42 +0900

    google oauth 추가

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,17 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: "ec2f7e66e962ed77ff33",
       clientSecret: "db9dd04f2a613a44c89265e4252e7a5799f2243b",
+    }),
+    GoogleProvider({
+      clientId:
+        "938523790700-hkc03p5of8jmkj7kgrce8qt39lunlg09.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
   ],
   secret: "2q4p23789ohasfldkjh234lkjhsdf",
